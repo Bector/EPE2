c2cdf3fe09cef4dd062a74ef01158fa5ead03de7
victorcastillojara <victorcastillo.and1@gmail.com>
2019-05-21 19:46:29 -0400

    ActionSheet Redes sociales

--- peliculas/src/app/tab1/tab1.page.ts
+++ peliculas/src/app/tab1/tab1.page.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
-import {NavController} from '@ionic/angular';
+import { NavController } from '@ionic/angular';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(public actionSheetController: ActionSheetController) { }
+
+  async HojaDeAccion() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Redes Sociales',
+      buttons: [{
+        text: 'Facebook',
+        icon: 'logo-facebook',
+        handler: () => {
+          window.open("https://www.facebook.com");
+        }
+      }, {
+        text: 'Twitter',
+        icon: 'logo-twitter',
+        handler: () => {
+          window.open("https://www.twitter.com");
+        }
+      }, {
+        text: 'Instagram',
+        icon: 'logo-instagram',
+        handler: () => {
+          window.open("https://www.instagram.com");
+        }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+
+}
